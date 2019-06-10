142b25c745d28eeb39d0ed8efd1d66f54d8dfff6
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-09 21:14:13 -0300

    58. Resolución de la tarea práctica #1 - Componente del héroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor( private _heroesService: HeroesService,
+               private router: Router
+  ) {
+  }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    // console.log(this.heroes);
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate( ['/heroe', idx] );
   }
 
 }
+
